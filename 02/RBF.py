6d193442c31246e6c00e0bd676cfa9b1a2bf1213
robertab <robertab@kth.se>
2018-02-08 14:39:40 +0100

    Find least errors (part 1)

--- 02/RBF.py
+++ 02/RBF.py
@@ -9,7 +9,6 @@ class RBF:
         K = np.zeros((len(X), nodes)).reshape(len(X), nodes)
         for node in range(nodes):
             K[:, node] = (np.exp(-((X - vec_mu[node])**2)/(2*vec_sigma[node]**2))).reshape(len(X), )
-        print(K)
 
         if learning_rule == 'least_squares':
             weights = self._least_squares(K, T)
@@ -17,6 +16,9 @@ class RBF:
 
     def _least_squares(self, K, T):
         return np.dot(np.linalg.pinv(K), T)
+
+    def error(self, Y, T):
+        return sum(np.absolute(Y - T)) / len(Y)
 
     def __str__(self):
         return "Radial Basis Function network"
--- 02/part_one.py
+++ 02/part_one.py
@@ -25,24 +25,30 @@ def main():
     R = RBF()
 
     # train on the data
-    nodes = 4
+    nodes = 7
     learning_rule = 'least_squares'
-    vec_mu = [.8, 2.2, 3.9, 5.4]
-    vec_sigma = [.5, .5, .5, .5]
-    Y = R.train(X_train, sin_T_train, nodes, vec_mu, vec_sigma, learning_rule)
+    vec_mu = [0., .78, 2.35, 3.9, 5.5, 5.89, 6.2]
+    # vec_sigma = [.51, .51, .51, .51]
+    sigmas = np.arange(.48, .63, 0.001)
+    delta = 0.01
+    errors = []
+    for sigma in sigmas:
+        vec_sigma = [sigma] * nodes
+        Y = R.train(X_train, sin_T_train, nodes, vec_mu, vec_sigma, learning_rule)
+        error = R.error(Y, sin_T_train)
+        errors.append(error)
+        print(error, sigma)
+        if error < delta:
+            plt.title("RBF network vs. sin(2*x). Error: {}".format(round(error[0], 8)))
+            plt.plot(X_train, sin_T_train, c='b')
+            plt.plot(X_train, Y, c='r')
+            plt.show()
 
-    plt.plot(X_train, sin_T_train)
-    plt.plot(X_train, Y)
-    plt.show()
-
-
-
-
-
-
-
-    print(R)
-    
+    print(min(error))
+    # plt.title("RBF network vs. sin(2*x). Error: {}".format(round(error[0], 3)))
+    # plt.plot(X_train, sin_T_train, c='b')
+    # plt.plot(X_train, Y, c='r')
+    # plt.show()
 
 
 if __name__ == '__main__':
