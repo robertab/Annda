8719ea58a09d25ec53a010f6537c2102a23836bd
herrmalte <dahlen.christopher@gmail.com>
2018-02-26 13:29:13 +0100

    added asynchrone recall

--- 03/HRNN.py
+++ 03/HRNN.py
@@ -5,9 +5,27 @@ class HRNN:
     def __init__(self, X):
         self.X = X
         self.weights = X.T.dot(X)
+        self.nneurons = len(X)
+        self.activations = None
 
-    def recall(self, patterns):
-        for i, pattern in enumerate(patterns):
-            patterns[i, :] = np.where(self.weights.dot(pattern) >= 0, 1, -1)
-        return patterns
+    def recall(self, patterns, epochs, synch=False):
+        self.activations = patterns
+        if synch:
+            for epoch in range(epochs):
+                for i,activation in enumerate(self.activations):
+                    self.activations[i,:] = np.where(activation.dot(self.weights) >= 0, 1, -1)
+            return self.activations
+        
+        else:
+            order = np.arange(self.nneurons) 
+            for epoch in range(epochs):
+                for i,activation in enumerate(self.activations):
+#                     if self.random:
+#                     np.random.shuffle(order) 
+                    for j in order:
+                        if np.sum(self.weights[j,:] * self.activations[i])>=0:
+                            self.activations[j] = 1
+                        else:
+                            self.activations[i] = -1
+            return self.activations
 
--- 03/part_one_seq.go
+++ 03/part_one_seq.go
@@ -1,29 +1,29 @@
 package main
-import (
-	"fmt"
-	"math/rand"
-// 	"sort"
-	"gonum.org/v1/gonum/mat"
-)
-
-func matPrint(X mat.Matrix){
-    fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
-    fmt.Printf("%v\n",fa)
-}
-
-type Neuron struct {
-    weights []float64
-}
-type Layer struct{
-   num_units []Neurons
-
-}
-
-func InitNeuron(neuron *Neuron, dimension int){
-    neuron.weights = make([]float64,dimension)
-    // init random weights
-    for index, _ := range neuron.weights {
-    // init random threshold weight
-        neuron.Weights[index] = rand.NormFloat64()
-    }
-}
+// import (
+// 	"fmt"
+// 	"math/rand"
+// // 	"sort"
+// 	"gonum.org/v1/gonum/mat"
+// )
+// 
+// func matPrint(X mat.Matrix){
+//     fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+//     fmt.Printf("%v\n",fa)
+// }
+// 
+// type Neuron struct {
+//     weights []float64
+// }
+// type Layer struct{
+//    num_units []Neurons
+// 
+// }
+// 
+// func InitNeuron(neuron *Neuron, dimension int){
+//     neuron.weights = make([]float64,dimension)
+//     // init random weights
+//     for index, _ := range neuron.weights {
+//     // init random threshold weight
+//         neuron.Weights[index] = rand.NormFloat64()
+//     }
+// }
