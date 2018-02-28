12e817d7a3d1756048ce34f3b763748a3aafcd99
robertab <robertab@kth.se>
2018-02-28 13:19:17 +0100

    Add comments

--- 03/task4.py
+++ 03/task4.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import math
 import random
 from HRNN import HRNN
+np.random.seed(1000)
 
-FILENAME = "pict.dat"
 
 def read_data(filename):
     data = np.zeros((9, 1024))
@@ -17,11 +17,12 @@ def read_data(filename):
     return data
 
 
-
-
 def add_noise(patterns, amt_of_noise):
+    # Calculate the percentages
     amt_of_noise = int(amt_of_noise * patterns.shape[1])
+    # Pick out a row vector of indices
     index = np.random.choice(patterns.shape[1], amt_of_noise, replace=False)
+    # Add some noise to the patterns by flipping the randomly selected units
     patterns[:, index] = patterns[:, index]*(-1)
     return np.copy(patterns)
 
@@ -29,28 +30,30 @@ def add_noise(patterns, amt_of_noise):
 def main():
     print("This is task 3.4 - Distortion Resistance")
     data = read_data(FILENAME)
+    # Select p1, p2, p3
     selected_patterns = data[0:3, :]
-    # print()
     noise = [.0001, .1, .2, .3, .4, .5, .6, .7, .80, .9, 1]
+    # Initialize the Hopfield RNN with three selected patterns from the pict dataset
     Network = HRNN(selected_patterns)
-    epochs = 1 # int(math.log2(len(selected_patterns)))
+    epochs = int(math.log2(len(selected_patterns)))
+    # Initialize a subplot object
     f, axarr = plt.subplots(2, 11)
     for i, amt_of_noise in enumerate(noise):
         new_selected_patterns = add_noise(np.copy(selected_patterns), amt_of_noise)
+        # Plot the noisy pattern 
         output = np.copy(new_selected_patterns[2])
         new_grid = output.reshape(32, 32)
         axarr[0, i].imshow(new_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
-
+        # Recall on all the patterns
         Network.recall(np.copy(new_selected_patterns[0:3, :]), epochs, True)
-
+        # Plot the predicted patterns
         output = np.copy(Network.activations[2])
         new_grid = output.reshape(32, 32)
         axarr[1, i].imshow(new_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
     plt.show()
 
-        # Network.plotter(Network.activations[0])
-
 
 
 if __name__ == '__main__':
+    FILENAME = "pict.dat"
     main()
