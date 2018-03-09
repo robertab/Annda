735f36f2bb6b371698789fc3eb32c39cbda3495c
robertab <robertab@kth.se>
2018-03-09 13:18:58 +0100

    Plot error convergence. Plot the reconstructions of each class

--- 04/task1.py
+++ 04/task1.py
@@ -32,28 +32,40 @@ def main():
 
     rbm = BernoulliRBM(random_state=0, verbose=True)
 
-    rbm.learning_rate = 0.01
-    rbm.n_iter = 30
-    rbm.n_components = 150
-
-    rbm.fit(X_train, y_train)
-    X_reconstructed = X_train.dot(rbm.components_.T).dot(rbm.components_)
-    # print(X_reconstructed.shape)
-    # print(rbm.components_.shape)
-    Jplt.imshow(original_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
-
-    # print()
-    # print("Logistic regression using RBM features:\n%s\n" % (
-    #         metrics.classification_report(
-    #                     y_test.reshape(NDATA,),
-    #                             classifier.predict(X_test))))
-    # # X_original = np.copy(X_train[10].reshape(28, 28))
-    original_grid = np.copy(X_train[2].reshape(28,28))
-    reconstructed_grid = np.copy(X_reconstructed[2].reshape(28,28))
-    plt.imshow(original_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
+    EPOCHS = 30
+    errors = np.empty((1, EPOCHS))
+    for epoch in range(EPOCHS):
+        rbm.learning_rate = 0.01
+        rbm.n_iter = epoch
+        rbm.n_components = 150
+        X_new = rbm.fit_transform(X_train, y_train)
+        X_reconstructed = X_new.dot(rbm.components_)
+        # Predict using a binary threshold. TODO: Logistic threshold
+        X_reconstructed = np.where(X_reconstructed >= 0, 1, 0)
+        errors[:, epoch] = np.sum(np.abs(X_reconstructed - X_train))
+    # f, axarr = plt.subplots(2, 10)
+    # classes = [11, 2, 8, 15, 7, 3, 0, 4, 31, 9]
+    # for j, cls in enumerate(classes):
+    #     original_grid = np.copy(X_train[cls].reshape(28,28))
+    #     reconstructed_grid = np.copy(X_reconstructed[cls].reshape(28,28))
+    #     axarr[0, j].imshow(original_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto") 
+    #     axarr[1, j].imshow(reconstructed_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
+    plt.plot(errors[0, :])
     plt.show()
-    plt.imshow(reconstructed_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
-    plt.show()
+    # print("\n\nMean error over {} pictures (false classified pixels): {}".format(NDATA, np.mean(errors)))
+
+    # # print()
+    # # print("Logistic regression using RBM features:\n%s\n" % (
+    # #         metrics.classification_report(
+    # #                     y_test.reshape(NDATA,),
+    # #                             classifier.predict(X_test))))
+    # # # X_original = np.copy(X_train[10].reshape(28, 28))
+    # original_grid = np.copy(X_train[11].reshape(28,28))
+    # reconstructed_grid = np.copy(X_reconstructed[11].reshape(28,28))
+    # plt.imshow(original_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
+    # plt.show()
+    # plt.imshow(reconstructed_grid, extent=(0, 32, 0, 32), interpolation='nearest', cmap=plt.cm.get_cmap('binary'), aspect="auto")
+    # plt.show()
 
 
 
